76033dca0dfc0e4dbe1f216065b2b52bc7fa45d2
David Li <david.li@duke.edu>
2020-10-17 20:17:48 -0400

    fixed stupid button

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	console.log("hi");
+})
+
+function startTimer() {
+	alert("timer");
+}
--- inputs.js
+++ inputs.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var Button = document.getElementById('input_button');
-	Button.addEventListener('click', function() {
+	var start_button = document.getElementById('start_button');
+	start_button.addEventListener('click', function() {
 		start_session();
+	});
+	var add_button = document.getElementById('add_button');
+	add_button.addEventListener('click', function() {
+		add_input();
 	});
 });
 
 function start_session() {
-	window.alert("hello");
+	//window.alert("Get studying!");
+	//window.close();
+	chrome.runtime.sendMessage({time: "hi"});
+}
+
+function add_input() {
+	var input = document.createElement("input");
+	input.type="text";
+	var container = document.getElementById("container");
+	container.appendChild(input);  
+	container.appendChild(document.createElement("br"));
 }
